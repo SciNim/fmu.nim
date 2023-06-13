f722397ff450a26a631079c3d8244e5d35d30d32
mantielero <josemaria.alkala@gmail.com>
2023-06-13 22:26:29 +0200

    cleaning unnecesary

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,7 @@
 rm inc.so
 rm inc.fmu
-nim c --nimcache:cache --deadcodeElim:off --app:lib -o:inc.so --mm:orc -f -d:release inc.nim
+# --nimcache:cache --deadcodeElim:off 
+nim c --app:lib -o:inc.so --mm:orc -f -d:release inc.nim
 cp inc.so fmusdk-master/fmu20/src/models/inc/fmu/binaries/linux64/inc.so
 cd fmusdk-master/fmu20/src/models/inc/fmu/
 zip -r ../../../../../../inc.fmu ./
