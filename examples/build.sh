f3105d08ad84a4b2afbec2baf356c93b5b24a506
mantielero <josemaria.alkala@gmail.com>
2023-11-27 22:27:10 +0100

    cleaning inc

--- examples/build.sh
+++
@@ -1,9 +0,0 @@
-rm inc.so
-rm inc.fmu
-# --nimcache:cache --deadcodeElim:off 
-nim c --app:lib -o:inc.so --mm:orc -f -d:release inc.nim
-cp inc.so fmusdk-master/fmu20/src/models/inc/fmu/binaries/linux64/inc.so
-cd fmusdk-master/fmu20/src/models/inc/fmu/
-zip -r ../../../../../../inc.fmu ./
-cd ../../../../../../
-./fmuCheck.linux64 inc.fmu
---
+++ examples/inc/run.sh
@@ -0,0 +1,2 @@
+nim c -r inc
+../fmuCheck.linux64 -h 0.2 inc.fmu
